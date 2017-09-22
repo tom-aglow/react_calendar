f083b02dc6486b261e9dfc4cf8c57cf3023d4e96
tom-aglow <tom@ohhhh.me>
2017-09-22 14:54:38 -0700

    handle user inputs into form field with react state

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -1,11 +1,25 @@
-var AppointmentForm = React.createClass({
+let AppointmentForm = React.createClass({
+  handleChange(e) {
+    let name = e.target.name;
+    let obj = {};
+    obj["input_" + name] = e.target.value;
+
+    this.props.onUserInput(obj);
+  },
+
   render() {
     return (
       <div>
           <h2>Make a new appointment</h2>
           <form>
-            <input name='title' placeholder='Appointment Title' />
-            <input name='apt_time' placeholder='Date and Time' />
+            <input name='title' placeholder='Appointment Title'
+                   value={this.props.input_title}
+                   onChange={this.handleChange}
+            />
+            <input name='apt_time' placeholder='Date and Time'
+                   value={this.props.input_apt_time}
+                   onChange={this.handleChange}
+            />
             <input type='submit' value='Make Appointment' />
           </form>
       </div>
--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,9 +1,25 @@
 let Appointments = React.createClass({
+  getInitialState() {
+    return {
+      appointments: this.props.appointments,
+      input_title: 'New title',
+      input_apt_time: 'Tomorrow at 9am'
+    }
+  },
+
+  handleUserInput(obj) {
+    this.setState(obj);
+  },
+
   render() {
     return (
       <div>
-        <AppointmentForm />
-        <AppointmentsList appointments={this.props.appointments} />
+        <AppointmentForm
+            input_title={this.state.input_title}
+            input_apt_time={this.state.input_apt_time}
+            onUserInput={this.handleUserInput}
+        />
+        <AppointmentsList appointments={this.state.appointments} />
       </div>
     )
   }
